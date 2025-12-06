eaee5376d72ac08d36e1cdafdd1395d9f464e8c7
jankotanime <jan.gasztold@gmail.com>
2025-12-06 17:41:00 +0100

    feature: working nginx on frontend and watch on api

--- hacknation/src/main/java/com/majami/hacknation/feature/global/HealthCheckController.java
+++ hacknation/src/main/java/com/majami/hacknation/feature/global/HealthCheckController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
   @GetMapping
   public ResponseEntity<String> getHealthcheck() {
-    return ResponseEntity.ok("Checked");
+    return ResponseEntity.ok("Checked, all good!");
   }
 }
